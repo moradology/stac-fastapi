434285523d7f18a62cfe2364901cfd996ed8cb90
Nathan Zimmerman <npzimmerman@gmail.com>
2021-06-29 10:29:13 -0500

    Revert extension change

--- stac_fastapi/api/stac_fastapi/api/openapi.py
+++ stac_fastapi/api/stac_fastapi/api/openapi.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 
+from stac_fastapi.api.config import ApiExtensions
 from stac_fastapi.types.config import ApiSettings
 
 
@@ -18,6 +19,15 @@ def config_openapi(app: FastAPI, settings: ApiSettings):
         )
 
         app.openapi_schema = openapi_schema
+
+        if settings.api_extension_is_enabled(ApiExtensions.fields):
+            openapi_schema["paths"]["/search"]["get"]["responses"]["200"]["content"][
+                "application/json"
+            ]["schema"] = {"$ref": "#/components/schemas/ItemCollection"}
+            openapi_schema["paths"]["/search"]["post"]["responses"]["200"]["content"][
+                "application/json"
+            ]["schema"] = {"$ref": "#/components/schemas/ItemCollection"}
+
         return app.openapi_schema
 
     app.openapi = custom_openapi
